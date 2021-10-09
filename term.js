a5b6cc1ae6f7c7befbc740a75856edbc29e50324
drdilyor <drdilyor@outlook.com>
2021-10-09 23:14:56 +0500

    More interactive term

--- term.js
+++ term.js
@@ -3,8 +3,42 @@ document.querySelector('.page-head__content').addEventListener('click', function
 })
 
 document.querySelector('.term__input').addEventListener('keydown', function(e) {
-  if (e.key == 'Enter' || e.key == ' ') {
-    document.querySelector('.term__input').blur()
-    document.querySelector('.page-head ~ .page').scrollIntoView()
+  const command = document.querySelector('.term__command')
+  switch (e.key) {
+    case 'Enter':
+      execute(command.textContent)
+      break
+    case 'Backspace':
+      command.textContent = command.textContent.slice(0, -1)
+      break
+    default:
+      if (/^[0-9a-zA-Z$~&%[{}(=*)+\]!#@\/,.<>;:  ]$/.test(e.key)) {
+        command.textContent += e.key
+      } else {
+        alert('The key you just pressed is unsupported to keep things simple.')
+      }
   }
 })
+
+/** @param command {string} */
+function execute(command) {
+  if (command.startsWith('cat ')) {
+    command = command.slice(4)
+    if (command.startsWith('./')) {
+      command = command.slice(2)
+    }
+    if (/^[0-9a-zA-Z._-]+$/.test(command)) {
+      document.querySelector('.term__input').blur()
+      const elem = document.getElementById(command)
+      if (elem == null) {
+        alert(`cat: ${command}: no element with such id.`)
+      } else {
+        elem.scrollIntoView()
+      }
+    } else {
+      alert('The command you just entered is unsupported to keep things simple.')
+    }
+  } else {
+    alert('The command you just entered is unsupported to keep things simple.')
+  }
+}
